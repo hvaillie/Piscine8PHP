6ad08af3153f98f46383721521b3443b07bed54c
unknown <hvaillie@student.42.fr>
2018-01-15 23:24:40 +0100

    PHP D05 ex00-ex03

---
+++ Day05/ex01/ex01.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `ft_table` (
+	`id` SMALLINT NOT NULL AUTO_INCREMENT,
+	`login` CHAR(8) NOT NULL DEFAULT 'toto',
+	`groupe` ENUM('staff', 'student', 'other') NOT NULL,
+	`date_de_creation` DATE NOT NULL,
+	PRIMARY KEY (`id`));
