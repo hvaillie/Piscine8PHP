a53da6424a8ad2e08ee1c9b633751ae82c9ee161
Hugues VAILLIER <hvaillie@e3r10p3.42.fr>
2018-01-17 22:15:50 +0100

    ex04 incomplet

---
+++ Day07/ex04/Lannister.class.php
@@ -0,0 +1,13 @@
+<?php
+
+class Lannister
+{
+	public function sleepWith($soeur)
+	{
+		if (get_parent_class($soeur) === get_class(%this))
+			print ("Not even if I'm drunk !".PHP_EOL);
+		else
+	}
+}
+
+?>
